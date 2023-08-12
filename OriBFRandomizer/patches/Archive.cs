20af8aa74bb7cb746abe9b37dd6382a552546b8d
Eiko <worst.mirari@gmail.com>
2023-08-12 13:02:06 -0700

    maybe this makes archive work?

--- OriBFRandomizer/patches/Archive.cs
+++ OriBFRandomizer/patches/Archive.cs
@@ -7,64 +7,30 @@ namespace OriBFRandomizer.patches
 {
     public static class ArchiveExt
     {
-        // private static readonly Lazy<AccessTools.FieldRef<Archive, bool>> MWrite =
-        //     new Lazy<AccessTools.FieldRef<Archive, bool>>(() => AccessTools.FieldRefAccess<Archive, bool>("m_write"));
-        //
-        // private static readonly Lazy<AccessTools.FieldRef<Archive, BinaryWriter>> MBinWriter =
-        //     new Lazy<AccessTools.FieldRef<Archive, BinaryWriter>>(() =>
-        //         AccessTools.FieldRefAccess<Archive, BinaryWriter>("m_binaryWriter"));
-        //
-        // private static readonly Lazy<AccessTools.FieldRef<Archive, BinaryReader>> MBinReader =
-        //     new Lazy<AccessTools.FieldRef<Archive, BinaryReader>>(() =>
-        //         AccessTools.FieldRefAccess<Archive, BinaryReader>("m_binaryReader"));
-
-
         public static void Serialize(this Archive archive, ref Dictionary<int, int> value)
         {
-            // value = archive.Serialize(value);
+            value = archive.Serialize(value);
         }
 
 
         public static Dictionary<int, int> Serialize(this Archive archive, Dictionary<int, int> value)
         {
-            // string text = "";
-            // if (MWrite.Value(archive))
-            // {
-            //     foreach (int key in value.Keys)
-            //     {
-            //         text = string.Concat(new string[]
-            //         {
-            //             text,
-            //             key.ToString(),
-            //             ":",
-            //             value[key].ToString(),
-            //             ","
-            //         });
-            //     }
-            //
-            //     text = text.TrimEnd(new char[]
-            //     {
-            //         ','
-            //     });
-            //     MBinWriter.Value(archive).Write(text);
-            //     return value;
-            // }
-            //
-            // value.Clear();
-            // text = MBinReader.Value(archive).ReadString();
-            // string[] array = text.Split(new char[]
-            // {
-            //     ','
-            // });
-            // for (int i = 0; i < array.Length; i++)
-            // {
-            //     string[] array2 = array[i].Split(new char[]
-            //     {
-            //         ':'
-            //     });
-            //     value[int.Parse(array2[0])] = int.Parse(array2[1]);
-            // }
-
+            var pairs = "";
+            if ((bool)AccessTools.Field(typeof(Archive), "m_write").GetValue(archive))
+            {
+                foreach(var key in value.Keys) 
+                    pairs += key.ToString() + ":"+value[key].ToString()+",";	
+                
+                pairs = pairs.TrimEnd(',');
+                ((BinaryWriter)AccessTools.Field(typeof(Archive), "m_binaryWriter").GetValue(archive)).Write(pairs);
+                return value;
+            }
+            value.Clear();
+            pairs = ((BinaryReader)AccessTools.Field(typeof(Archive), "m_binaryWriter").GetValue(archive)).ReadString();
+            foreach(var pair in pairs.Split(',')) {
+                var kandv = pair.Split(':');
+                value[int.Parse(kandv[0])] = int.Parse(kandv[1]);							
+            }
             return value;
         }
     }
