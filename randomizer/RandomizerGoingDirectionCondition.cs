a8f0f300a584ee85e1658c0173cdbbbc7fd6e302
AsmPrgmC3 <asmprgmc3@gmail.com>
2026-08-05 02:58:41 +0200

    Move `RandomizerGoingDirectionCondition.cs` to the correct folder

--- randomizer/RandomizerGoingDirectionCondition.cs
+++ randomizer/RandomizerGoingDirectionCondition.cs
@@ -2,12 +2,12 @@ using Game;
 
 public class RandomizerGoingDirectionCondition : Condition {
     public override bool Validate(IContext context) {
-        if (left) {
+        if (Left) {
             return Characters.Sein.PlatformBehaviour.PlatformMovement.LocalSpeedX < 0f;
         }
 
         return Characters.Sein.PlatformBehaviour.PlatformMovement.LocalSpeedX > 0f;
     }
 
-    public bool left;
+    public bool Left;
 }
