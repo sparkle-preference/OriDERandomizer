57ab16a705fa6a22e661da61f118e7d72b2e44a2
AsmPrgmC3 <asmprgmc3@gmail.com>
2026-08-09 20:23:57 +0200

    Switch GameWorldArea to dnSpy version

--- modified_classes/GameWorldArea.cs
+++ modified_classes/GameWorldArea.cs
@@ -3,47 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class GameWorldArea : MonoBehaviour {
-    private const float PIXELS_PER_UNIT = 5f;
-    public List<WorldMapIcon> Icons = new List<WorldMapIcon>();
-    public MessageProvider AreaName;
-    public MessageProvider LowerAreaName;
-    public string AreaNameString;
-    public CageStructureTool CageStructureTool;
-    public Transform BoundingTransform;
-    public Texture WorldMapTexture;
-    public string AreaIdentifier = string.Empty;
-    public CageStructureTool BoundaryCage;
-    public Condition VisitableCondition;
-
+public class GameWorldArea : MonoBehaviour
+{
     public Bounds Bounds => new Bounds(BoundingTransform.position, BoundingTransform.localScale);
 
     public Rect BoundingRect =>
-        new Rect {
+        new Rect
+        {
             width = BoundingTransform.lossyScale.x,
             height = BoundingTransform.lossyScale.y,
             center = BoundingTransform.position
         };
 
-    public bool InsideFace(Vector3 worldPosition) {
-        return BoundaryCage.FindFaceAtPositionFaster(BoundaryCage.transform.InverseTransformPoint(worldPosition)) != null;
+    public bool InsideFace(Vector3 worldPosition)
+    {
+        var vector = BoundaryCage.transform.InverseTransformPoint(worldPosition);
+        return BoundaryCage.FindFaceAtPositionFaster(vector) != null;
     }
 
-    [Serializable]
-    public class WorldMapIcon {
-        public MoonGuid Guid;
-        public WorldMapIconType Icon;
-        public Vector2 Position;
-        public bool IsSecret;
+    private const float PIXELS_PER_UNIT = 5f;
 
-        public WorldMapIcon(SceneMetaData.WorldMapIcon worldMapIcon) {
+    public List<WorldMapIcon> Icons = new List<WorldMapIcon>();
+
+    public MessageProvider AreaName;
+
+    public MessageProvider LowerAreaName;
+
+    public string AreaNameString;
+
+    public CageStructureTool CageStructureTool;
+
+    public Transform BoundingTransform;
+
+    public Texture WorldMapTexture;
+
+    public string AreaIdentifier = string.Empty;
+
+    public CageStructureTool BoundaryCage;
+
+    public Condition VisitableCondition;
+
+    [Serializable]
+    public class WorldMapIcon
+    {
+        public WorldMapIcon(SceneMetaData.WorldMapIcon worldMapIcon)
+        {
             Guid = new MoonGuid(worldMapIcon.Guid);
             Position = worldMapIcon.Position;
             Icon = worldMapIcon.Icon;
             IsSecret = worldMapIcon.IsSecret;
         }
 
-        public WorldMapIcon() {
+        public WorldMapIcon()
+        {
         }
+
+        public MoonGuid Guid;
+
+        public WorldMapIconType Icon;
+
+        public Vector2 Position;
+
+        public bool IsSecret;
     }
 }
