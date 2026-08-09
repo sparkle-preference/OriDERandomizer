002e850b92731a5cd3ef30e4029d227b4a904368
AsmPrgmC3 <asmprgmc3@gmail.com>
2026-08-09 20:36:16 +0200

    Reformat RandomizerGoingDirection
    (forgot that earlier as it wasn't part of the solution)

--- randomizer/RandomizerGoingDirectionCondition.cs
+++ randomizer/RandomizerGoingDirectionCondition.cs
@@ -1,22 +1,16 @@
-using System;
 using Game;
-using UnityEngine;
 
-public class RandomizerGoingDirectionCondition : Condition
-{
-	public override bool Validate(IContext context)
-	{
+public class RandomizerGoingDirectionCondition : Condition {
+    public override bool Validate(IContext context) {
         if (left) {
             return Characters.Sein.PlatformBehaviour.PlatformMovement.LocalSpeedX < 0f;
         } else {
             return Characters.Sein.PlatformBehaviour.PlatformMovement.LocalSpeedX > 0f;
         }
-        
-	}
+    }
 
-	public RandomizerGoingDirectionCondition()
-	{
-	}
-    
+    public RandomizerGoingDirectionCondition() {
+    }
+
     public bool left = false;
 }
